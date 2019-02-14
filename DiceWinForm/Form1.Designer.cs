537af9d629316763817b42991257ffc65a434626
chris <christopherscaudill@gmail.com>
2019-02-14 18:57:49 -0500

    game works, added reset button

--- DiceWinForm/Form1.Designer.cs
+++ DiceWinForm/Form1.Designer.cs
@@ -40,6 +40,9 @@
             this.label10Point = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4RollNbr = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -160,11 +163,41 @@
             this.label11.TabIndex = 11;
             this.label11.Text = "Click Here To Roll";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "# of Rolls:";
+            // 
+            // label4RollNbr
+            // 
+            this.label4RollNbr.AutoSize = true;
+            this.label4RollNbr.Location = new System.Drawing.Point(92, 231);
+            this.label4RollNbr.Name = "label4RollNbr";
+            this.label4RollNbr.Size = new System.Drawing.Size(0, 13);
+            this.label4RollNbr.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(285, 450);
+            this.Controls.Add(this.label4RollNbr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label10Point);
@@ -178,7 +211,7 @@
             this.Controls.Add(this.label2Game);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Simple Craps";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,6 +231,9 @@
         private System.Windows.Forms.Label label10Point;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4RollNbr;
     }
 }
 
--- DiceWinForm/Form1.cs
+++ DiceWinForm/Form1.cs
@@ -18,10 +18,112 @@ namespace DiceWinForm
         }
 
         Random rng = new Random();
+        Random rng2 = new Random();
+        int roll = 0;
+        int wins = 0;
+        int loses = 0;
+        int game = 0;
+        int point = 0;
+        int croll = 0;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //0
+            roll = 0;
+
+            if (point != 0)
+            {
+                croll++;
+                label4RollNbr.Text = croll.ToString();
+                Roll2();
+            }
+            else
+            {
+                croll++;
+                label4RollNbr.Text = croll.ToString();
+                Roll1();
+            }
+            
+            
+
+        }
+
+
+        private void Roll1()
+        {
+            game++;
+            label2Game.Text = game.ToString();
+
+            roll = rng.Next(1, 7) + rng2.Next(1, 7);
+            label8Roll.Text = roll.ToString();
+
+            if(roll == 7 || roll == 11)
+            {
+                Win();
+            }
+            else if(roll == 2 || roll == 3 || roll == 12)
+            {
+                Lose();
+            }
+            else
+            {
+                point = roll;
+                label10Point.Text = point.ToString();
+            }
+        }
+
+        private void Roll2()
+        {
+            roll = rng.Next(1, 7) + rng2.Next(1, 7);
+            label8Roll.Text = roll.ToString();
+
+            if(roll == 7)
+            {
+                Lose();
+            }
+            else if(roll == point)
+            {
+                Win();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void Win()
+        {
+            wins++;
+            label4Wins.Text = wins.ToString();
+            point = 0;
+            croll = 0;
+            label10Point.Text = "";
+            label4RollNbr.Text = "";
+        }
+
+        private void Lose()
+        {
+            loses++;
+            label6Loses.Text = loses.ToString();
+            point = 0;
+            croll = 0;
+            label10Point.Text = "";
+            label4RollNbr.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            roll = 0;
+            wins = 0;
+            loses = 0;
+            game = 0;
+            point = 0;
+            croll = 0;
+            label2Game.Text = "";
+            label4Wins.Text = "";
+            label6Loses.Text = "";
+            label8Roll.Text = "";
+            label10Point.Text = "";
+            label4RollNbr.Text = "";
         }
     }
 }
